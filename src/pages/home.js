3261871edf6054d928966fee72c044004d774478
Oliwia <legendhm@gmail.com>
2021-07-12 17:09:01 +0100

    FT - this dum dum thing refuses to work

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,43 +1,67 @@
-import React from 'react';
+import React ,{ useState } from 'react' ;
 
-class Home extends React.Component{
-    render(){
-            return(
-        <div>
-            <div class="main-list">
-                <form class="list" action="database.php" method="POST">
-                    <div class="details">
-                        <div class="login">
-                            <span class="info">Login:</span>
-                            <input type="text" name="login" placeholder="Enter your login..."/> 
-                        </div>
-                        <div class="login">
-                            <span class="info">Password:</span>
-                            <input type="text" name="login" placeholder="Enter your password..."/> 
-                        </div>
-                        <div class="login">
-                            <span class="info">Link url:</span>
-                            <input type="password" name="login"/>
-                        </div>
-                    </div>
-                    <div></div>
-                    <div class="description">
-                        <span class="infod">Link description:</span>
-                        <textarea name="link-desc"></textarea>                 
-                    </div>
-                    <div class="bottom">
-                        <select name="type">
-                            <option value="type1">Image</option>
-                            <option value="type2">Youtube link</option>
-                            <option value="type3">Other link</option>
-                        </select>
-                        <button type="submit" name="submit">Submit</button>
-                    </div>
-                </form>
-            </div>            
-        </div>
-            )
+function Home (){
+    const [login,SetLogin] = useState("");
+
+    const [loginErr,SetLoginErr] = useState({});
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        const isValid = formValidation(); 
     }
+
+    const formValidation = () =>{
+        const loginErr = {};
+        let isValid = true;
+
+        if(login.trim().lenght < 5){
+            loginErr.sad = "ajsdasjda";
+            isValid = false;
+        }
+
+        SetLoginErr(loginErr);
+        return isValid;
+    }
+
+
+    return(
+    <div>
+    <div class="main-list">
+        <form class="list" action="database.php" method="POST" onSubmit={onSubmit}>
+            <div class="details">
+                <div class="login">
+                    <span class="info">Login:</span>
+                    <input type="text" name="login" placeholder="Enter your login..." onChange={(e)=>{SetLogin(e.target.value)}}/> 
+                </div>
+                {Object.keys(loginErr).map((key)=>{
+                    return <div style={{color : "red"}}>{loginErr[key]}</div>
+                })}
+                <div class="login">
+                    <span class="info">Password:</span>
+                    <input type="text" name="login" placeholder="Enter your password..."/> 
+                </div>
+                <div class="login">
+                    <span class="info">Link url:</span>
+                    <input type="password" name="login"/>
+                </div>
+            </div>
+            <div></div>
+            <div class="description">
+                <span class="infod">Link description:</span>
+                <textarea name="link-desc"></textarea>                 
+            </div>
+            <div class="bottom">
+                <select name="type">
+                    <option value="type1">Image</option>
+                    <option value="type2">Youtube link</option>
+                    <option value="type3">Other link</option>
+                </select>
+                <button type="submit" name="submit">Submit</button>
+            </div>
+        </form>
+    </div>            
+</div>
+    )
 }
 
 export default Home;
